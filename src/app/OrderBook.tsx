d5fa04812c7384c1b01e536775944f6d6ddf4f19
NTh-Hai <thehai318@gmail.com>
2023-05-12 22:38:44 +0700

    feat: adding grouping select box

--- src/app/OrderBook.tsx
+++ src/app/OrderBook.tsx
@@ -2,152 +2,186 @@
 
 import { useEffect, useRef, useState } from "react";
 
+// Order represent value in order: price , size, total
 type Order = [number, number, number?];
 
-const message = {
-  event: "subscribe",
-  feed: "book_ui_1",
-  product_ids: ["PI_XBTUSD"],
+const coinOption = {
+  XBT: {
+    message: {
+      event: "subscribe",
+      feed: "book_ui_1",
+      product_ids: ["PI_XBTUSD"],
+    },
+    gap: [0.5, 1, 2.5],
+  },
+  ETH: {
+    message: {
+      event: "subscribe",
+      feed: "book_ui_1",
+      product_ids: ["PI_ETHUSD"],
+    },
+    gap: [0.05, 0.1, 0.25],
+  },
 };
 
 export default function OrderBook() {
+  const [coin, setCoin] = useState<"XBT" | "ETH">("XBT");
   const [feedData, setFeedData] = useState<{ bids: Order[]; asks: Order[] }>({
     bids: [],
     asks: [],
   });
-  const maxSize = useRef({ maxBid: 0, maxAsk: 0 });
+  const [gap, setGap] = useState<number>(coinOption[coin].gap[0]);
   useEffect(() => {
     const ws = new WebSocket("wss://www.cryptofacilities.com/ws/v1");
 
     ws.onopen = () => {
-      ws.send(JSON.stringify(message));
+      ws.send(JSON.stringify(coinOption[coin].message));
     };
 
     let bids: Order[] = [];
-    let maxBidSize = 0;
     let asks: Order[] = [];
-    let maxAskSize = 0;
 
     ws.onmessage = (event) => {
       const data = JSON.parse(event.data);
       if (data.feed === "book_ui_1_snapshot") {
         // Processing snapshot Data
         bids = data.bids;
-        maxBidSize = getMaxSize(bids);
         asks = data.asks;
-        maxAskSize = getMaxSize(asks);
       } else if (data.feed === "book_ui_1" && data.event !== "subscribed") {
         // Processing updated delta data
         data.bids.forEach((deltaOrder: Order) => {
           if (deltaOrder[1] === 0) {
             bids = bids.filter((bid) => bid[0] !== deltaOrder[0]);
-            maxBidSize = getMaxSize(bids);
           } else {
             bids = bids.filter((bid) => bid[0] !== deltaOrder[0]);
             bids.push(deltaOrder);
-            maxBidSize = getMaxSize(bids);
           }
         });
         data.asks.forEach((deltaOrder: Order) => {
           if (deltaOrder[1] === 0) {
             asks = asks.filter((ask) => ask[0] !== deltaOrder[0]);
-            maxAskSize = getMaxSize(asks);
           } else {
             asks = asks.filter((ask) => ask[0] !== deltaOrder[0]);
             asks.push(deltaOrder);
-            maxAskSize = getMaxSize(asks);
           }
         });
       }
-      bids.sort((a, b) => b[0] - a[0]);
-      asks.sort((a, b) => a[0] - b[0]);
-      // Adding total size value
-      let totalBid = 0;
-      bids.forEach((bid) => {
-        totalBid += bid[1];
-        bid[2] = totalBid;
-      });
-      let totalAsk = 0;
-      asks.forEach((ask) => {
-        totalAsk += ask[1];
-        ask[2] = totalAsk;
-      });
+      bids.sort((a, b) => b[0] - a[0]); // Ascending order
+      asks.sort((a, b) => a[0] - b[0]); // Descending order
+
       setFeedData({ bids, asks });
-      maxSize.current = { maxBid: maxBidSize, maxAsk: maxAskSize };
     };
 
     return () => {
       ws.close();
     };
-  }, []);
+  }, [coin]);
+
+  let groupBids = groupingPrice(feedData.bids, gap);
+  let groupAsks = groupingPrice(feedData.asks, gap);
+
+  // Adding total size value
+  let totalBid = 0;
+  groupBids.forEach((bid) => {
+    totalBid += bid[1];
+    bid[2] = totalBid;
+  });
+  let totalAsk = 0;
+  groupAsks.forEach((ask) => {
+    totalAsk += ask[1];
+    ask[2] = totalAsk;
+  });
+
   return (
-    <div className="flex items-start mt-10">
-      <table className="h-fit">
-        <thead>
-          <tr>
-            <th className="w-[150px] text-right">Total Bid</th>
-            <th className="w-[150px] text-right">Bid Size</th>
-            <th className="w-[150px] text-right">Bid Price</th>
-          </tr>
-        </thead>
-        <tbody>
-          {feedData.bids.map((bid) => {
-            return (
-              <tr key={bid[0]}>
-                <td className="w-[150px] text-right">
-                  {bid[2]!.toLocaleString()}
-                </td>
-                <td className="w-[150px] text-right">
-                  {bid[1].toLocaleString()}
-                </td>
-                <td className="w-[150px] text-right">
-                  {bid[0].toLocaleString(undefined, {
-                    minimumFractionDigits: 2,
-                  })}
-                </td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
-      <table className="h-fit">
-        <thead>
-          <tr>
-            <th className="w-[150px] text-right">Ask Price</th>
-            <th className="w-[150px] text-right">Ask Size</th>
-            <th className="w-[150px] text-right">Total Ask</th>
-          </tr>
-        </thead>
-        <tbody>
-          {feedData.asks.map((ask) => {
-            return (
-              <tr key={ask[0]}>
-                <td className="w-[150px] text-right">
-                  {ask[0].toLocaleString(undefined, {
-                    minimumFractionDigits: 2,
-                  })}
-                </td>
-                <td className="w-[150px] text-right">
-                  {ask[1].toLocaleString()}
-                </td>
-                <td className="w-[150px] text-right">
-                  {ask[2]!.toLocaleString()}
-                </td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+    <div>
+      <div className="flex justify-around p-5">
+        <select
+          className="bg-slate-800 p-2 rounded-sm"
+          onChange={(e) => setGap(parseFloat(e.target.value))}
+        >
+          {coinOption[coin].gap.map((num) => (
+            <option value={num} key={num}>
+              Group {num.toFixed(2)}
+            </option>
+          ))}
+        </select>
+        <button className="p-2 bg-purple-800 rounded-lg">Toggle Feed</button>
+      </div>
+      <div className="flex items-start">
+        <table className="h-fit">
+          <thead>
+            <tr>
+              <th className="w-[150px] text-right">Total Bid</th>
+              <th className="w-[150px] text-right">Bid Size</th>
+              <th className="w-[150px] text-right">Bid Price</th>
+            </tr>
+          </thead>
+          <tbody>
+            {groupBids.map((bid) => {
+              return (
+                <tr key={bid[0]}>
+                  <td className="w-[150px] text-right">
+                    {bid[2]!.toLocaleString()}
+                  </td>
+                  <td className="w-[150px] text-right">
+                    {bid[1].toLocaleString()}
+                  </td>
+                  <td className="w-[150px] text-right">
+                    {bid[0].toLocaleString(undefined, {
+                      minimumFractionDigits: 2,
+                    })}
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+        <table className="h-fit">
+          <thead>
+            <tr>
+              <th className="w-[150px] text-right">Ask Price</th>
+              <th className="w-[150px] text-right">Ask Size</th>
+              <th className="w-[150px] text-right">Total Ask</th>
+            </tr>
+          </thead>
+          <tbody>
+            {groupAsks.map((ask) => {
+              return (
+                <tr key={ask[0]}>
+                  <td className="w-[150px] text-right">
+                    {ask[0].toLocaleString(undefined, {
+                      minimumFractionDigits: 2,
+                    })}
+                  </td>
+                  <td className="w-[150px] text-right">
+                    {ask[1].toLocaleString()}
+                  </td>
+                  <td className="w-[150px] text-right">
+                    {ask[2]!.toLocaleString()}
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
 
-function getMaxSize(orders: Order[]) {
-  let max: number = 0;
+function groupingPrice(orders: Order[], gap: number) {
+  // Only work if orders has been sorted
+  let newOrders: Order[] = [];
   orders.forEach((order) => {
-    if (order[1] > max) {
-      max = order[1];
+    let priceDiff = order[0] % gap;
+    if (
+      newOrders[newOrders.length - 1] &&
+      newOrders[newOrders.length - 1][0] === order[0] - priceDiff
+    ) {
+      newOrders[newOrders.length - 1][1] += order[1];
+    } else {
+      newOrders.push([order[0] - priceDiff, order[1]]);
     }
   });
-  return max;
+  return newOrders;
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,9 +8,11 @@ export default function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-start p-24">
-      <div className="uppercase font-bold text-[3rem]">OrderBook</div>
+      <div className="uppercase font-bold text-[3rem] leading-none">
+        OrderBook
+      </div>
       <button
-        className="rounded-lg bg-pink-700 px-3 py-2"
+        className="rounded-lg bg-pink-700 px-3 py-2 my-10"
         onClick={() => setIsOrderbookOpen(!isOrderbookOpen)}
       >
         Toggle Orderbook
